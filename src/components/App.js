a8c2cfa7b4a32eeabfafa3c09178b8ee1ae4abb7
rodrigocqb <rodrigocortibarros@gmail.com>
2022-09-14 20:42:48 -0300

    Login page layout done

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyle from "../styles/GlobalStyle";
+import Login from "./Login";
 import SignUp from "./SignUp";
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
       <GlobalStyle />
       <Routes>
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/login" element="" />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,14 @@
 import { useState } from "react";
 import { ThreeDots } from "react-loader-spinner";
 import { Link, useNavigate } from "react-router-dom";
-import styled from "styled-components";
 import { Form } from "../common/Form";
-import { Title } from "../common/Title";
-import { postSignUp } from "../services/everest";
+import { Span, TitleWrapper } from "./SignUp";
 
-export default function SignUp() {
+export default function Login() {
   const [disabled, setDisabled] = useState(false);
   const [form, setForm] = useState({
-    name: "",
     email: "",
     password: "",
-    confirmPassword: "",
   });
 
   const navigate = useNavigate();
@@ -26,42 +22,14 @@ export default function SignUp() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    if (form.confirmPassword !== form.password) {
-      return alert("The passwords do not match");
-    }
     setDisabled(true);
-    postSignUp({
-      name: form.name,
-      email: form.email,
-      password: form.password,
-    })
-      .then(() => navigate("/login"))
-      .catch((err) => {
-        console.log(err);
-        alert(
-          "There was an error when trying to sign up.\nCheck your inputs and try again."
-        );
-        setDisabled(false);
-      });
+    console.log(form);
   }
 
   return (
     <main>
       <TitleWrapper>EVEREST</TitleWrapper>
       <Form disabled={disabled} onSubmit={handleSubmit}>
-        <input
-          placeholder="Name"
-          name="name"
-          type="text"
-          disabled={disabled}
-          onChange={(e) =>
-            handleForm({
-              value: e.target.value,
-              name: e.target.name,
-            })
-          }
-          required
-        />
         <input
           placeholder="Email"
           name="email"
@@ -88,19 +56,6 @@ export default function SignUp() {
           }
           required
         />
-        <input
-          placeholder="Confirm your password"
-          name="confirmPassword"
-          type="password"
-          disabled={disabled}
-          onChange={(e) =>
-            handleForm({
-              value: e.target.value,
-              name: e.target.name,
-            })
-          }
-          required
-        />
         <button type="submit">
           {disabled ? (
             <ThreeDots
@@ -110,23 +65,13 @@ export default function SignUp() {
               ariaLabel="three-dots-loading"
             />
           ) : (
-            <p>Sign Up</p>
+            <p>Sign In</p>
           )}
         </button>
       </Form>
-      <Link to="/login">
-        <Span>Already a member? Log in!</Span>
+      <Link to="/sign-up">
+        <Span>New here? Create an account!</Span>
       </Link>
     </main>
   );
 }
-
-export const TitleWrapper = styled(Title)`
-  margin-top: 15vh;
-`;
-
-export const Span = styled.span`
-  font-size: 15px;
-  font-weight: 700;
-  color: #22223b;
-`;
