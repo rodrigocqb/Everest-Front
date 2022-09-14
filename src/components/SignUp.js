f8d366419c28973c5a8900c5995b0fde77c6f148
rodrigocqb <rodrigocortibarros@gmail.com>
2022-09-14 19:21:53 -0300

    Fixed upper case on password input name

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -76,7 +76,7 @@ export default function SignUp() {
         />
         <input
           placeholder="Password"
-          name="Password"
+          name="password"
           type="password"
           disabled={disabled}
           onChange={(e) =>
