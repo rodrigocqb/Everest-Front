1f523db4e60e29d6ef93788f441454e2bb872bb5
rodrigocqb <rodrigocortibarros@gmail.com>
2022-09-18 00:23:08 -0300

    Cart icon now shows the ammount of items on it

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,27 @@
+import { useContext, useEffect } from "react";
 import { Main } from "../common/Main";
+import CartContext from "../contexts/CartContext";
 import { useLocal } from "../hooks/useLocal";
+import { getCart } from "../services/everest";
 import Header from "./Header";
 import ProductCards from "./ProductCards";
 
 export default function Home() {
+  const { setCart } = useContext(CartContext);
+
   useLocal();
+
+  useEffect(() => {
+    getCart()
+      .then((res) => {
+        setCart(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, [setCart]);
+
   return (
     <>
       <Header />
